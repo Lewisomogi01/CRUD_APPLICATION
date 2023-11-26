5034a74961112d313d8bc21ab20f0b720b7f1c1e
Dev Lewis <lewisomogi01@gmail.com>
2023-11-26 15:56:54 +0000

    my third commit

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import CreateUserForm, LoginForm
+from .forms import CreateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
 from .models import Record
+
+from django.contrib import messages
 
 def home(request):
     return render(request, 'webapp/index.html')
@@ -16,7 +18,9 @@ def register(request):
         if form.is_valid():
             form.save()
             # Redirect to the desired page after registration
-            return redirect('dashboard')
+            messages.success(request, "Account created successfully!")
+            
+            return redirect('login')
             
     context = {'form': form}
     return render(request, 'webapp/register.html', context=context)
@@ -35,6 +39,9 @@ def my_login(request):
             if user is not None:
                 login(request, user)
                 # Redirect to the dashboard after login
+                messages.success(request, "You have logged in successfully!!")
+            
+                
                 return redirect('dashboard')
 
     context = {'form': form}
@@ -52,10 +59,86 @@ def dashboard(request):
     
     return render(request, 'webapp/dashboard.html', context=context)
 
+#--create or add record
+@login_required(login_url='login')
+def create_record(request):
+    
+    form = CreateRecordForm()
+    
+    if request.method == "POST":
+        
+        form = CreateRecordForm(request.POST)
+        
+        if form.is_valid():
+            
+            form.save()
+            
+            messages.success(request, "Record was created successfully!")
+            
+            
+            return redirect("dashboard")
+        
+    context = {'form': form}
+    
+    return render(request,'webapp/create-record.html', context=context)
+    
+#update record
+@login_required(login_url='login')
+def update_record(request,pk):
+    
+    record= Record.objects.get(id=pk)
+    
+    form= UpdateRecordForm(instance=record)
+    
+    if request.method== "POST":
+        
+        form = UpdateRecordForm(request.POST, instance=record)
+        
+        if form.is_valid():
+            
+            form.save()
+            
+            messages.success(request, "Your record was updated successfully!")
+            
+            
+            return redirect("dashboard")
+    context = {'form': form}
+    
+    return render(request, 'webapp/update-record.html', context=context)
+       
+    
+#read or view a single record
+
+@login_required(login_url='login')
+def singular_record (request,pk):
+    
+    all_records = Record.objects.get(id=pk)
+    
+    context = {'record':all_records}
+    
+    return render(request, 'webapp/view-record.html', context)
+
+#delete a record
+
+@login_required(login_url='login')
+def delete_record (request,pk):
+    
+    record = Record.objects.get(id=pk)
+    
+    record.delete()
+    
+    messages.success(request, "Your record was deleted successfully!")
+            
+    
+    return redirect ("dashboard")
+    
 # Logout
 def user_logout(request):
     if request.user.is_authenticated:
         logout(request)
+        
+        messages.success(request, "You logged out successfully!")
+            
     return redirect("login")
 
        
